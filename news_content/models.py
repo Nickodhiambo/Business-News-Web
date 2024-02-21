bb1b70c62e53b3bd224fde9bd5dd44ff71bf8eb5
Nick <nodhiambo01@gmail.com>
2024-02-21 17:20:45 +0300

    Create model

--- news_content/models.py
+++ news_content/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Content(models.Model):
+    """Defines business content data schema"""
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    link = models.URLField()
+    pub_date = models.DateTimeField()
+    guid = models.CharField(max_length=100)
+    site_name = models.CharField(max_length=100)
+    site_logo = models.URLField(default=None)
+
+    def __str__(self) -> str:
+        """Returns a string representation of model"""
+        return f"{self.site_name}: {self.title}"
