f7d71b8e9bd5474c0780e0c160d78947e11a2e9e
Nick <nodhiambo01@gmail.com>
2024-02-21 18:19:54 +0300

    Build testcase && modified models

--- news_content/tests.py
+++ news_content/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Content
+from django.utils import timezone
 
 # Create your tests here.
+class ContentTestCase(TestCase):
+    """Unit tests for Content model"""
+    def setUp(self):
+        """Creates a test instance"""
+        self.content = Content.objects.create(
+                title="Business News",
+                description="Top business news across Africa",
+                link="https://some-link.com",
+                pub_date=timezone.now(),
+                guid="odbdnn-mndhnk-nnsjc",
+                site_name="Business Center",
+                )
+
+    def test_model_fields(self):
+        """Tests that model fields values are correctly assigned"""
+        self.assertEqual(self.content.link, "https://some-link.com")
+        self.assertEqual(self.content.title, "Business News")
+        self.assertEqual(self.content.description, "Top business news across Africa")
+
+    def test_to_string(self):
+        """Test model string representation"""
+        self.assertEqual(
+                str(content), "Business Center: Business News"
+                )
